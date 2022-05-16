05921e7da684497563d6fb21b85e71ade4e81e22
iremoncdam1 <iremoncdam1@gmail.com>
2022-05-17 01:10:10 +0200

    Estructura del código. Añadidos espacios para mejorar la legibilidad.

--- src/Juego.java
+++ src/Juego.java
@@ -9,49 +9,49 @@ public class Juego
 {
     public static void main(String args[])
     {
-        Jugador p1=new Jugador();
-        Jugador p2=new Jugador();
+        Jugador p1 = new Jugador();
+        Jugador p2 = new Jugador();
         boolean finDeJuego = false;
         Integer rondasJugadas = 0;    // Número de rondas jugadas
-        Integer exitosJugador1=p1.éxitos;
-        Integer exitosJugador2=p2.éxitos;
+        Integer exitosJugador1 = p1.éxitos;
+        Integer exitosJugador2 = p2.éxitos;
         Integer empates = 0;
         String opcionJugador1, opcionJugador2;
         
         // Bucle de juego
         do {
             System.out.println("***** Ronda: " + rondasJugadas+" *********************\n");
-            System.out.println("Numero de empates: "+ empates + "\n");
+            System.out.println("Numero de empates: " + empates + "\n");
             opcionJugador1=p1.opcionAlAzar();
             System.out.println("Jugador 1: " + opcionJugador1+"\t Jugador 1 - Partidas ganadas: " + exitosJugador1);
             opcionJugador2 = p2.opcionAlAzar();
             System.out.println("Jugador 2: " + opcionJugador2+"\t Jugador 2 - Partidas ganadas: " + exitosJugador2);
             
-            if((opcionJugador1.equals("piedra"))&&(opcionJugador2.equals("papel"))) {
+            if ((opcionJugador1.equals("piedra")) && (opcionJugador2.equals("papel"))) {
                 System.out.println("Jugador 2 GANA");
                 exitosJugador2 = ++p2.éxitos;
-            } else if((opcionJugador1.equals("papel"))&&(opcionJugador2.equals("piedra"))) {
+            } else if ((opcionJugador1.equals("papel")) && (opcionJugador2.equals("piedra"))) {
                 exitosJugador1 = ++p1.éxitos;
                 System.out.println("Jugador 1 GANA");
-            } else if((opcionJugador1.equals("piedra"))&&(opcionJugador2.equals("tijeras"))) {
+            } else if ((opcionJugador1.equals("piedra")) && (opcionJugador2.equals("tijeras"))) {
                 exitosJugador1 = ++p1.éxitos;
                 System.out.println("Jugador 1 GANA");
-            } else if((opcionJugador1.equals("tijeras"))&&(opcionJugador2.equals("piedra"))) {
+            } else if ((opcionJugador1.equals("tijeras")) && (opcionJugador2.equals("piedra"))) {
                 exitosJugador2 = ++p2.éxitos;
                 System.out.println("Jugador 2 GANA");
-            } else if((opcionJugador1.equals("tijeras"))&&(opcionJugador2.equals("papel"))) {
+            } else if ((opcionJugador1.equals("tijeras")) && (opcionJugador2.equals("papel"))) {
                 exitosJugador1 = ++p1.éxitos;
                 System.out.println("Jugador 1 GANA");
-            } else if((opcionJugador1.equals("papel"))&&(opcionJugador2.equals("tijeras"))) {
+            } else if ((opcionJugador1.equals("papel")) && (opcionJugador2.equals("tijeras"))) {
                 exitosJugador2 = ++p2.éxitos;
                 System.out.println("Jugador 2 GANA");
             }
-            if(opcionJugador1==opcionJugador2) {
+            if(opcionJugador1 == opcionJugador2) {
                 empates++;
                 System.out.println("\n\t\t\t Empate \n");
             }
             rondasJugadas++;
-            if((p1.éxitos>=3)||(p2.éxitos>=3)) {
+            if((p1.éxitos >= 3) || (p2.éxitos >= 3)) {
                 finDeJuego=true;
                 System.out.println("FIN DEL JUEGO!!");
             }
@@ -71,15 +71,15 @@ class Jugador{
     {
         String opcion="";
         Integer c = (int)(Math.random()*3);
-        switch(c){
+        switch(c) {
             case 0:
-            	opcion=("piedra");
+            	opcion = ("piedra");
                 break;
             case 1:
-            	opcion=("papel");
+            	opcion = ("papel");
                 break;
             case 2:
-            	opcion=("tijeras");
+            	opcion =("tijeras");
         }
         return opcion;
     }
