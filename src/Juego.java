c586ce19fe5edda109a16084bfdd689db818e6cf
iremoncdam1 <iremoncdam1@gmail.com>
2022-05-17 00:48:31 +0200

    Nombres e identificadores. Modificada variable 'EXITOS_jugador2' por 'exitosJugador2'.

--- src/Juego.java
+++ src/Juego.java
@@ -14,7 +14,7 @@ public class Juego
         boolean finDeJuego = false;
         Integer Rondasjugadas = 0;    // Número de rondas jugadas
         Integer exitosJugador1=p1.éxitos;
-        Integer EXITOS_jugador2=p2.éxitos;
+        Integer exitosJugador2=p2.éxitos;
         Integer EmPaTeS = 0;
         String opcion_JUGADOR1, opcion_JUGADOR2;
         
@@ -26,12 +26,12 @@ public class Juego
             opcion_JUGADOR1=p1.opcion_al_azar();
             System.out.println("Jugador 1: " + opcion_JUGADOR1+"\t Jugador 1 - Partidas ganadas: " + exitosJugador1);
             opcion_JUGADOR2 = p2.opcion_al_azar();
-            System.out.println("Jugador 2: " + opcion_JUGADOR2+"\t Jugador 2 - Partidas ganadas: " + EXITOS_jugador2);
+            System.out.println("Jugador 2: " + opcion_JUGADOR2+"\t Jugador 2 - Partidas ganadas: " + exitosJugador2);
             
             if((opcion_JUGADOR1.equals("piedra"))&&(opcion_JUGADOR2.equals("papel")))
             {
                 System.out.println("Jugador 2 GANA");
-                EXITOS_jugador2 = ++p2.éxitos;
+                exitosJugador2 = ++p2.éxitos;
                 
             }
             else if((opcion_JUGADOR1.equals("papel"))&&(opcion_JUGADOR2.equals("piedra")))
@@ -46,7 +46,7 @@ public class Juego
             }
             else if((opcion_JUGADOR1.equals("tijeras"))&&(opcion_JUGADOR2.equals("piedra")))
             {
-            	EXITOS_jugador2 = ++p2.éxitos;
+                exitosJugador2 = ++p2.éxitos;
                 System.out.println("Jugador 2 GANA");
             }
             else if((opcion_JUGADOR1.equals("tijeras"))&&(opcion_JUGADOR2.equals("papel")))
@@ -56,7 +56,7 @@ public class Juego
             }
             else if((opcion_JUGADOR1.equals("papel"))&&(opcion_JUGADOR2.equals("tijeras")))
             {
-            	EXITOS_jugador2 = ++p2.éxitos;
+                exitosJugador2 = ++p2.éxitos;
                 System.out.println("Jugador 2 GANA");
             }
             if(opcion_JUGADOR1==opcion_JUGADOR2)
